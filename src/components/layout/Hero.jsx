cc545a1ab084b8de08c80bd914fb7c21191c00cf
Okimchi <basilio.miko123@gmail.com>
2025-05-25 01:51:05 +0800

    created hero section

---
+++ src/components/layout/Hero.jsx
@@ -0,0 +1,10 @@
+export default function Hero(){
+    return(
+        <main className="w-[80%] flex flex-col items-center border">
+            <h1 className="text-[44px]">Hi, I'm Miko Basilio</h1>
+            <span>Full Stack Developer</span>
+            <p>I build things for the web.</p>
+            
+        </main>
+    )
+}
