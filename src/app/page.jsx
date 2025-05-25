989f1a7e5fa02c525c7a4c008f1fd40ea5f0f790
Okimchi <basilio.miko123@gmail.com>
2025-05-26 00:06:50 +0800

    added about me section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,13 @@
-import Image from "next/image";
-import NavBar from "@/components/layout/Navbar";
-import Hero from "@/components/layout/Hero";
+import NavBar from "@/components/layout/navbar";
+import Hero from "@/components/layout/hero";
+import AboutMe from "@/components/layout/about";
 
 export default function Home() {
   return (
     <main className="w-full relative flex flex-col items-center">
       <NavBar />
       <Hero />
+      <AboutMe />
     </main>
   );
 }
