f103cfa0dbb4f9fc89d5d52652cb57ce0eef0efb
Okimchi <basilio.miko123@gmail.com>
2025-05-25 19:46:37 +0800

    added font to main layout

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,9 +1,10 @@
 import "@/styles/globals.css";
+import { rubik } from "@/styles/fonts";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className={`${rubik.className}`}>
         {children}
       </body>
     </html>
