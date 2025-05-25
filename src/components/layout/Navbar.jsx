afd1a64e740f7cfc97633fc24db2c6247a2cb597
Okimchi <basilio.miko123@gmail.com>
2025-05-25 19:46:49 +0800

    fix navbar

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,23 +1,24 @@
-import { AlignJustify } from "lucide-react"
+import { AlignJustify } from "lucide-react";
+import { Button } from "../ui/button";
 
-export default function NavBar(){
-    return(
-        <nav className="sticky top-0 flex w-[80%] bg-[#1e1d1c] justify-between py-5">
-            <h1 className="font-medium text-[22px]">
-                MIKO BASILIO
-            </h1>
-            <ul className="hidden md:flex md:gap-4 text-[20px]">
-                <li>
-                    About
-                </li>
-                <li>
-                    Projects
-                </li>
-                <li>
-                    Experience
-                </li>
-            </ul>
-            <AlignJustify color="white" className="md:hidden" />
-        </nav>
-    )
+export default function NavBar() {
+  return (
+    <nav className="sticky top-0 flex w-[80%] dark bg-background items-center justify-between py-5">
+      <h1 className="font-medium text-[20px]">MIKO BASILIO</h1>
+      <ul className="hidden md:flex gap-5 tracking-widest text-[18px]">
+        <li>About</li>
+        <li>Projects</li>
+        <li>Experience</li>
+      </ul>
+
+      <Button
+        variant="outline"
+        className="hidden md:block border-prismo active:border-white"
+      >
+        Resume
+      </Button>
+
+      <AlignJustify color="white" className="md:hidden" />
+    </nav>
+  );
 }
