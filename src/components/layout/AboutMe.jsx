0c3f5b495cd04e64f11d9063e3c6dc8f94811e0b
Okimchi <basilio.miko123@gmail.com>
2025-05-26 23:12:05 +0800

    done aboutme

--- src/components/layout/AboutMe.jsx
+++ src/components/layout/AboutMe.jsx
@@ -1,21 +1,47 @@
-import Reveal from "../motion/reveal"
+import Reveal from "../motion/reveal";
 
-export default function AboutMe(){
-    return(
-        <section className="h-[400] w-[80%] flex justify-between items-center">
-            <Reveal delayProp={0.2} className="w-[40%]">
-              <article >
-                <h1 className="header">About me</h1>
-                <p className="text-justify font-light">I'm a Web Developer with a strong passion for crafting simple but engaging web experience, particularly on the front end. I enjoy turning ideas into working websites, and I'm always set on finishing projects well using modern tools for great results. </p>
-            </article>  
-            </Reveal>
-            
-            <Reveal delayProp={0.2} className="w-[40%]">
-              <figure>
-                <h1 className="header">Skills</h1>
-            </figure>  
-            </Reveal>
-            
-        </section>
-    )
+export default function AboutMe() {
+  const skills = [
+    "NextJS",
+    "ReactJS",
+    "Javascript",
+    "Tailwind",
+    "Figma",
+    "UI/UX",
+    "NodeJS",
+    "ExpressJS",
+    "MongoDB",
+    "Firebase",
+    "REST API",
+  ];
+  return (
+    <section className="h-[400] md:h-fit py-8 w-[80%] gap-5 flex flex-col md:flex-row items-start">
+      <Reveal delayProp={0.2} className="w-full">
+        <article>
+          <h1 className="header mt-1">
+            About <strong className="prismo"> me </strong>
+          </h1>
+          <p className="text-justify font-light">
+            I'm a Web Developer with a strong passion for crafting simple but
+            engaging web experience, particularly on the front end. I enjoy
+            turning ideas into working websites, and I'm always set on finishing
+            projects well using modern tools for great results.
+          </p>
+        </article>
+      </Reveal>
+
+      <Reveal delayProp={0.2} className="w-full">
+        <div className="flex flex-col justify-start gap-2">
+          <h1 className="header">Skills</h1>
+          <div className="grid grid-cols-3 gap-2 sm:grid-cols-4 md:grid-cols-3 lg:grid-cols-4">
+            {skills.map((item) => (
+              <figure key={item} className="text-[15px] bg-pink-300 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-50 flex px-3 py-2 items-center justify-center ">
+                {item}
+              </figure>
+            ))}
+          </div>
+        </div>
+      </Reveal>
+    </section>
+  );
 }
