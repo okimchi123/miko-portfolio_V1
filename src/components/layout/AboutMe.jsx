c247e497a3c4a14debb690ebd65cc35b7410ce0b
Okimchi <basilio.miko123@gmail.com>
2025-05-28 01:35:10 +0800

    finished aboutme section

--- src/components/layout/AboutMe.jsx
+++ src/components/layout/AboutMe.jsx
@@ -1,5 +1,6 @@
 import Reveal from "../motion/reveal";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function AboutMe() {
   const skills = [
@@ -16,7 +17,7 @@ export default function AboutMe() {
     "REST API",
   ];
   return (
-    <section className="h-[400] md:h-fit py-8 w-[80%] gap-20 flex flex-col md:flex-row items-start">
+    <section className="h-fit border md:h-[500px] py-20 md:py-8 w-[90%] md:w-[80%] gap-20 md:gap-20 flex flex-col md:flex-row items-center">
       <Reveal delayProp={0.2} className="w-full">
         <article>
           <h1 className="header mt-1">
@@ -24,13 +25,35 @@ export default function AboutMe() {
           </h1>
           <p className="text-justify font-light mb-4">
             I'm a Web Developer with a strong passion for crafting simple but
-            engaging web experience, particularly on the <strong className="prismo font-medium">Frontend</strong>. I enjoy
+            engaging web experience, particularly on the{" "}
+            <strong className="prismo font-medium">Frontend</strong>. I enjoy
             turning ideas into working websites, and I'm always set on finishing
-            projects well using <strong className="prismo font-medium">modern tools</strong> for great results.
+            projects well using{" "}
+            <strong className="prismo font-medium">modern tools</strong> for
+            great results.
           </p>
-          <figure className="flex gap-2">
-        <Image src="/github.svg" width="40" height="40" alt="github" />
-        <Image src="/linkedin.svg" width="40" height="40" alt="linkedin" />
+          <figure className="flex gap-3">
+            <Link href="https://github.com/okimchi123" target="_blank">
+              <Image
+                src="/github.svg"
+                width="35"
+                height="35"
+                alt="github"
+                className=""
+              />
+            </Link>
+            <Link
+              href="https://www.linkedin.com/in/miko-basilio-251348361/"
+              target="_blank"
+            >
+              <Image
+                src="/linkedin.svg"
+                width="40"
+                height="40"
+                alt="linkedin"
+                className=""
+              />
+            </Link>
           </figure>
         </article>
       </Reveal>
@@ -40,7 +63,10 @@ export default function AboutMe() {
           <h1 className="header">Skills</h1>
           <div className="grid grid-cols-3 gap-2 sm:grid-cols-4 md:grid-cols-3 lg:grid-cols-4">
             {skills.map((item) => (
-              <figure key={item} className="text-[15px] bg-pink-300 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-50 flex px-3 py-2 items-center justify-center ">
+              <figure
+                key={item}
+                className="text-[15px] bg-pink-300 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-50 flex px-3 py-2 items-center justify-center "
+              >
                 {item}
               </figure>
             ))}
