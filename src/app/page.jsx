82b5a9e7932c4bbb95f1685137e99d23e16afa40
Okimchi <basilio.miko123@gmail.com>
2025-05-28 01:34:41 +0800

    starting project section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,15 @@
 import NavBar from "@/components/layout/Navbar";
 import Hero from "@/components/layout/Hero";
 import AboutMe from "@/components/layout/AboutMe";
+import Projects from "@/components/layout/Projects";
 
 export default function Home() {
   return (
-    <main className="w-full relative flex flex-col items-center">
+    <main className="w-full relative flex flex-col items-center gap-4">
       <NavBar />
       <Hero />
       <AboutMe />
+      <Projects />   
     </main>
   );
 }
---
+++ src/components/layout/Projects.jsx
@@ -0,0 +1,7 @@
+export default function Projects(){
+    return(
+        <section className="w-[90%] md:w-[80%] h-[400px]">
+            <h1 className="header">Projects</h1>
+        </section>
+    )
+}
