5fe7f84bb4585817c9f672d15eca43e7bc02ba45
Okimchi <basilio.miko123@gmail.com>
2025-05-30 23:51:33 +0800

    created project.model

---
+++ src/models/project.model.jsx
@@ -0,0 +1,21 @@
+const postData = [
+    {
+      Image: "/healthharmony.png",
+      Title: "HealthHarmony",
+      Role: "Frontend Developer",
+      Description:
+        "HealthHarmony is an information management system designed for the DHVSU Medical and Dental Services, replacing the previously paper-based system. It enables students, teachers, and staff to securely access their medical information. The system also streamlines clinic operations by allowing staff to efficiently manage patient records and inventory, improving organization and accessibility within the clinic.",
+      Github:"https://github.com/IamIsthill/healthharmony",
+      Website:"",
+    },
+    {
+      Image: "/inventorysystem.png",
+      Title: "Express Inventory System",
+      Role: "Full Stack Developer",
+      Description: "This system is designed to provide a centralized and efficient platform for managing company assets, with built-in data analytics and asset distribution tracking. It allows users to easily monitor asset availability, detect missing items, and identify responsible handlers through distribution logs.",
+      Github:"https://github.com/okimchi123/InventorySystem",
+      Website:"https://express-inventory-system.vercel.app",
+    },
+  ];
+
+export {postData};
