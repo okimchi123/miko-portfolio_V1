407e3155663054615db71d353a27b3849fee6310
Okimchi <basilio.miko123@gmail.com>
2025-06-06 23:56:21 +0800

    added the resume component to ui

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,12 +1,15 @@
 import Sidebar from "./Sidebar";
 import { Button } from "@/components/ui/button";
 import NavMotion from "@/components/motion/navmotion";
+import ResumeModal from "./Resume";
 import Link from "next/link";
 
 export default function NavBar() {
   //add pdf react on resume button
   return (
     <nav className="z-1000 sticky -top-0.5 flex w-[91%] md:w-[81%] dark bg-background select-none items-center justify-between py-5">
+      <ResumeModal />
+      
       <NavMotion />
       <Link href="#start">
       <h1 className="font-medium text-[18px] tracking-widest cursor-pointer">
