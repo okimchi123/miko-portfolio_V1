8dfa11dfb25e1894e9d412bb2f47cc45c5c813a2
Okimchi <basilio.miko123@gmail.com>
2025-06-08 22:50:19 +0800

    added resume modal component to navbar

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,15 +1,19 @@
+'use client'
 import Sidebar from "./Sidebar";
 import { Button } from "@/components/ui/button";
 import NavMotion from "@/components/motion/navmotion";
 import ResumeModal from "./Resume";
 import Link from "next/link";
+import { useState } from "react";
 
 export default function NavBar() {
-  //add pdf react on resume button
+
+  const [resumeModal, setResumeModal] = useState(false);
+
   return (
     <nav className="z-1000 sticky -top-0.5 flex w-[91%] md:w-[81%] dark bg-background select-none items-center justify-between py-5">
      
-      
+      <ResumeModal open={resumeModal} close={()=>setResumeModal(false)} />
       <NavMotion />
       <Link href="#start">
       <h1 className="font-medium text-[18px] tracking-widest cursor-pointer">
@@ -29,6 +33,7 @@ export default function NavBar() {
           </li>
         </Link>
         <Button
+        onClick={()=>{setResumeModal(true)}}
         variant="outline"
         className="hidden md:block border-prismo active:border-white"
       >
