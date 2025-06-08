7c0760a533902c905cd309caffa8cca11c7e4c3c
Okimchi <basilio.miko123@gmail.com>
2025-06-08 22:50:29 +0800

    added resume modal component to sidebar

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -1,11 +1,25 @@
+"use client";
+import { useState } from "react";
 import { AlignJustify } from "lucide-react";
-import { Sheet, SheetContent, SheetTrigger, SheetHeader, SheetTitle, SheetClose } from "@/components/ui/sheet";
+import {
+  Sheet,
+  SheetContent,
+  SheetTrigger,
+  SheetHeader,
+  SheetTitle,
+  SheetClose,
+} from "@/components/ui/sheet";
 import Link from "next/link";
 import { Button } from "../ui/button";
+import ResumeModal from "./Resume";
 
 export default function Sidebar() {
+  const [resumeModal, setResumeModal] = useState(false);
+
   return (
     <Sheet>
+      <ResumeModal open={resumeModal} close={() => setResumeModal(false)} />
+
       <SheetTrigger className="md:hidden">
         <AlignJustify color="white" />
       </SheetTrigger>
@@ -16,26 +30,30 @@ export default function Sidebar() {
         <ul className="flex justify-center items-center gap-5 tracking-widest text-[16px]">
           <SheetClose asChild>
             <Link href="#about">
-            <li className="cursor-pointer hover:-translate-y-1 transition-all hover:prismo">
-              About
-            </li>
-          </Link>
+              <li className="cursor-pointer hover:-translate-y-1 transition-all hover:prismo">
+                About
+              </li>
+            </Link>
           </SheetClose>
-          
+
           <SheetClose asChild>
             <Link href="#projects">
-            <li className="cursor-pointer hover:-translate-y-1 transition-all hover:prismo">
-              Projects
-            </li>
-          </Link>
+              <li className="cursor-pointer hover:-translate-y-1 transition-all hover:prismo">
+                Projects
+              </li>
+            </Link>
           </SheetClose>
-          
-          <Button
-            variant="outline"
-            className="block border-prismo active:border-white"
-          >
-            Resume
-          </Button>
+          <SheetClose asChild>
+            <Button
+              onClick={() => {
+                setResumeModal(true);
+              }}
+              variant="outline"
+              className="block border-prismo active:border-white"
+            >
+              Resume
+            </Button>
+          </SheetClose>
         </ul>
       </SheetContent>
     </Sheet>
