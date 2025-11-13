0f966c1f0ff309aa9e78177cf7411f5430e09962
Okimchi <basilio.miko123@gmail.com>
2025-11-13 22:44:52 +0800

    removed prismo subtitle

--- src/models/project.model.jsx
+++ src/models/project.model.jsx
@@ -18,7 +18,6 @@ const postData = [
     },
     {
       Image: "/prismo.png",
-      subTitle: "still updating",
       Title: "Prismo",
       Role: "Full Stack Developer",
       Description: "Prismo is a social media mock-up I built to hone my skills in full-stack web development. Using Next.js for the frontend and Firebase for the backend, I implemented key features including user profile creation and management, post management (creating, editing, and deleting posts), and a real-time chat feature. This project served as a comprehensive learning experience in building a scalable web application.",
